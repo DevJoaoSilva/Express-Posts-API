2f14e9ab15ac339dc83c69f0b0c658f923b9aa5b
Joao Silva <joaosilvadev.93@gmail.com>
2024-04-09 16:42:57 +0100

    refactor: ts config and start/build commands

---
+++ dist/routes.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const routes = (0, express_1.Router)();
+routes.get('/', (req, res) => res.send('Hello world'));
+exports.default = routes;
