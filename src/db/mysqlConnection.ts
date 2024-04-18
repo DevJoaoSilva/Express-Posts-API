58eab2af8e4120f158d690d13d8070892d407c20
Joao Silva <joaosilvadev.93@gmail.com>
2024-04-18 16:29:00 +0100

    refact: mysql connection, add env variables

--- src/db/mysqlConnection.ts
+++ src/db/mysqlConnection.ts
@@ -1,6 +1,11 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
-export const mysqlConnection = new Sequelize('express_posts', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
+export const mysqlConnection = new Sequelize(
+    process.env.MYSQL_DB as string,
+    process.env.MYSQL_USER as string,
+    process.env.MYSQL_PASS as string,
+    {
+        host: process.env.MYSQL_HOST as string,
+        dialect: 'mysql',
+    }
+);
